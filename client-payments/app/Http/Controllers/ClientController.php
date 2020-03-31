c5db181e47804549c1eb58ea0c67081449526452
Manuel <manuelespv@gmail.com>
2020-03-31 02:04:34 -0300

    Create migrations, models and controllers
    Store and Create method for Client

---
+++ client-payments/app/Http/Controllers/ClientController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Client;
+use Illuminate\Support\Facades\Log;
+
+class ClientController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        $client = new Client();
+        $client->email = $request->email;
+
+        $now = new \DateTime();
+        $now->format('Y-m-d');
+        $client->join_date = $now;
+
+
+
+        $client->save();
+
+        #Example of Log message. Look the file /app/storage/laravel.log
+        Log::info($client);
+    }
+
+    public function list()
+    {
+
+        $clients = Client::select('clients.id', 'clients.email', 'clients.join_date')->orderBy('clients.id', 'desc')->get();
+        return [
+            'clients' => $clients
+        ];
+    }
+}
